0b6c4fb68f0d4b97dce66e3bd917bc367c19028a
emily <mle4d@me.com>
2019-07-08 13:59:50 -0700

    technology is hard

---
+++ test/index.test.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+
+describe('dont ask why', () => {
+
+  it('doesnt ask why', done => {
+    const str = words.toString;
+     for(i = 0; i <)
+
+  
+    
+        return;
+      });
+    })
